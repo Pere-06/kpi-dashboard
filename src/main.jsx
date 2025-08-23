9ff1597e38f8f2f030b9d4461bf592fe1496d7d1
Pere-06 <pcapar06@gmail.com>
2025-08-23 20:10:30 +0200

    Protect app with Clerk SignedIn/SignedOut

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; 
+import { ClerkProvider, SignedIn, SignedOut, SignIn, SignUp } from "@clerk/clerk-react";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const pk = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+const AuthScreen = () => {
+  const [hash, setHash] = React.useState(window.location.hash);
+  React.useEffect(() => {
+    const onHash = () => setHash(window.location.hash);
+    window.addEventListener("hashchange", onHash);
+    return () => window.removeEventListener("hashchange", onHash);
+  }, []);
+  const isSignUp = hash.includes("sign-up");
+  return (
+    <div className="min-h-screen grid place-items-center bg-zinc-50 dark:bg-zinc-950">
+      {isSignUp ? <SignUp routing="hash" signInUrl="/sign-in" /> : <SignIn routing="hash" signUpUrl="/sign-up" />}
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={pk}>
+    <SignedIn><App /></SignedIn>
+    <SignedOut><AuthScreen /></SignedOut>
+  </ClerkProvider>
 );
